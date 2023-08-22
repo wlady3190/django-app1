13bc565b54a99fd4998ae162fbd9f52398a8a2a5
wlady3190 <wlady_3190@hotmail.com>
2023-08-21 22:59:07 -0500

    good practices

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-38svu206)xdga$i43u(fs&tq%^iljvv$=3#j(j^wfrtpy2oy_h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
